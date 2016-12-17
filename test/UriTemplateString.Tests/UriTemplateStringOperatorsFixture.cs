8aa43c594620859763f3dbead31119c3ad64a215
tpluscode <tpluscode@users.noreply.github.com>
2016-12-17 23:01:21 +0100

    template concatenation

---
+++ test/UriTemplateString.Tests/UriTemplateStringOperatorsFixture.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace UriTemplateString.Tests
+{
+    public class UriTemplateStringOperatorsFixture
+    {
+        [Fact]
+        public void Should_concatenate_two_templates()
+        {
+            // given
+            var left = new UriTemplateString("http://example.com/{area}/");
+            var right = new UriTemplateString("{controller}{/action}");
+
+            // when
+            var concat = left + right;
+
+            // then
+            concat.ToString().Should().Be("http://example.com/{area}/{controller}{/action}");
+        }
+
+        [Fact]
+        public void Should_concatenate_template_with_string()
+        {
+            // given
+            var left = new UriTemplateString("http://example.com/{area}/");
+            var right = "{controller}{/action}";
+
+            // when
+            var concat = left + right;
+
+            // then
+            concat.ToString().Should().Be("http://example.com/{area}/{controller}{/action}");
+        }
+
+        [Fact]
+        public void Should_concatenate_string_with_template()
+        {
+            // given
+            var left = "http://example.com/{area}/";
+            var right = new UriTemplateString("{controller}{/action}");
+
+            // when
+            var concat = left + right;
+
+            // then
+            concat.ToString().Should().Be("http://example.com/{area}/{controller}{/action}");
+        }
+    }
+}

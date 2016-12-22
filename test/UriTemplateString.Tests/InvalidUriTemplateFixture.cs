64e2b575b0518e1308f0a75cae8ebc7fc9bd0097
tpluscode <tpluscode@users.noreply.github.com>
2016-12-22 21:15:00 +0100

    add some negative tests

---
+++ test/UriTemplateString.Tests/InvalidUriTemplateFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace UriTemplateString.Tests
+{
+    public class InvalidUriTemplateFixture
+    {
+        [Fact]
+        public void Should_throw_when_created_from_empty_string()
+        {
+            Assert.Throws<ArgumentException>(() => new UriTemplateString(string.Empty));
+        }
+
+        [Fact]
+        public void Should_throw_when_created_from_template_with_unclosed_expression()
+        {
+            Assert.Throws<ArgumentException>(() => new UriTemplateString("{/unclosed"));
+        }
+
+        [Fact]
+        public void Should_throw_when_created_from_template_with_unsupported_expression_operator()
+        {
+            Assert.Throws<ArgumentException>(() => new UriTemplateString("{'unclosed}"));
+        }
+    }
+}
---
+++ test/UriTemplateString.Tests/OperatorFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UriTemplateString.Spec;
+using Xunit;
+
+namespace UriTemplateString.Tests
+{
+    public class OperatorFixture
+    {
+        [Fact]
+        public void Should_throw_when_constructed_with_unsupported_operator_character()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Operator.FromString("_"));
+        }
+
+        [Theory]
+        [InlineData("=")]
+        [InlineData(",")]
+        [InlineData("!")]
+        [InlineData("@")]
+        [InlineData("|")]
+        public void Should_throw_when_constructed_with_operator_reserved_for_future_use(string op)
+        {
+            Assert.Throws<NotImplementedException>(() => Operator.FromString(op));
+        }
+    }
+}
